93c8331fd50b67e0b4dc912ab9a6836029d77e07
Phillip Wood <PWood9@uclan.ac.uk>
2025-02-11 10:01:47 +0000

    added a nest counter to util, changed util to a passed variable to each function instead of a global

--- Programming Assignment 2/Source.cpp
+++ Programming Assignment 2/Source.cpp
@@ -1,5 +1,7 @@
-#include <iostream>
+ï»¿#include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
 
 /*
 Programming Assignment 2 - Payroll System - Phillip Wood
@@ -12,51 +14,50 @@ Allow a user to enter a pay file and recursively show pay
 for each employee in the file
 
 Calculating Income Tax:
-	assuming £2,000 monthly income.
-	£2,000 * 12 = £24,000 (annual income)
-	£24,000 - £12,570 = £11,430 (taxable income)
-	£11,430 * 0.20 = £2,286 (annual income tax)
-	£2,286 / 12 = £190.50 (monthly tax deduction).
+	assuming Â£2,000 monthly income.
+	Â£2,000 * 12 = Â£24,000 (annual income)
+	Â£24,000 - Â£12,570 = Â£11,430 (taxable income)
+	Â£11,430 * 0.20 = Â£2,286 (annual income tax)
+	Â£2,286 / 12 = Â£190.50 (monthly tax deduction).
 */
 
 
 /*
-TODO:	/  Done
-		// Doing
+TODO:	/  Doing
+		// Done
 
 - Create Utility functions /
-	* Read Text /
-	* Write Text /
+	* Read Text 
+	* Write Text 
 	* Menu Template /
+		- Potentially make title update automatically to centre with subtitle
 	* Question Template /
 - Create Main Functions
 	* Welcome Screen
 	* Calculate Pay / Income Tax
+
+
+
 */
-
-
+class utility;
 //Function Definitions ------------------------------------
-
-
-
-
-
-//---------------------------------------------------------
-
-//Separate Functions -----------------------------------------------
-
+void Hello(utility& util);
+void Hi(utility& util);
+void Welcome(utility& util);
 
 
 
 
 //---------------------------------------------------------
+
 
 //Classes -------------------------------------------------
 
 //Utility functions e.g. Read, Write
-class util {
+class utility {
 public:
-
+	//The amount of nested menus the user is currently in;
+	int nest = 0;
 	/*
 	little weird having a class nested in a class but it's cleaner than having it separate so o well
 	Will store a question e.g.
@@ -70,9 +71,61 @@ public:
 	*/
 	class Question {
 	public:
+		std::string title = "What Would You Like To Do?";
+		std::vector<std::string> options;
+		//options will by default have a back option
+		Question(std::string title = "What Would you Like to do?", std::vector<std::string> options = {"Back"}) : title(title), options(options) {
+			this->title = title;
+			this->options = options;
+		}
+		
 		//Ask the question
-		void ask() {
-			return;
+		int ask(utility& util) {
+			if (options.back() != "Back") {
+				if (util.nest == 0) {
+					options.push_back("Quit");
+				}
+				else {
+					options.push_back("Back");
+				}
+			}
+			//display title of question
+			std::cout << std::endl << title << std::endl;
+			//display options
+			for (int i = 0; i <= options.size()-1; i++) {
+				// 0 - {Answer}
+				std::cout << i+1 << " - " << options[i] << std::endl;
+			}
+
+			//get user input
+			std::string inputstr;
+			int inputint = 0;
+			//loop until we get an answer
+			while (inputint == 0) {
+				//get the input
+				std::getline(std::cin, inputstr);
+				//convert string to int and check if it is a number
+				inputint = std::strtol(inputstr.c_str(), NULL, 0);
+				//it isn't a number
+				std::cout << inputint;
+				if (inputint == 0 or inputint > options.size() or inputint < 1) {
+					std::cout << "Entry is not a valid!";
+					system("pause");
+					break;
+				}
+				//it is a number
+				else {
+					//is the input the last element if so we want to quit or go back a menu
+					if (inputint == options.size()) {
+						//it is
+						return 0;
+					}
+					else {
+						return inputint;
+					}
+				
+				}
+			}
 		}
 	};
 
@@ -81,7 +134,7 @@ public:
 	Options:
 		filename (string) *Required* - The path to the file you want to read.
 	*/
-	std::string Read(std::string filename) {
+	std::string Read(std::string filename, utility& util) {
 		return "";
 	}
 
@@ -90,44 +143,101 @@ public:
 	Options:
 		filename (string) *Required* - The path to the file you want to write to (will create the file if it doesn't exist).
 	*/
-	bool Write(std::string filename) {
+	bool Write(std::string filename, utility& util) {
 		return 0;
 
 	}
 
 	/*
-	Create and Show the user a standardised menu
+	Create and Show the user a standardised menu, content and question are technically optional but you should probably use at least one
 	Options:
 		title (string) *Required* - The Title of the menu e.g. Welcome
 		subtitle (string) *Optional* - a subtitle displayed under the title, could be useful for nested menus
-		description (string) *Optional* - a description displayed in the menu, could tell information about the menu.
+		content (string) *Optional* - Content in the menu e.g. could be a paragraph or just a sentence
+		question *Optional* - A Question to ask the user, e.g. what menu to move to
+
+	There will always be a back option in the question so no need to add one
 	*/
-	void Menu(std::string title, std::string subtitle = "", std::string description = "", Question question = Question()) {
-		return;
+	int Menu(std::string title, std::string subtitle = "", std::string content = "", Question question = Question(), utility util = utility()) {
+		std::cout << "============ " << title << " ============ " << nest << std::endl;
+		std::cout << "-------- " << subtitle << "--------" << std::endl;
+		std::cout << content;
+		return question.ask(util);
 	}
+
 	
 	
 
 	
 };
-
-
-
-
 //---------------------------------------------------------
-
 //Constants -----------------------------------------------
 
 
+//---------------------------------------------------------
+
+
+//Separate Functions -----------------------------------------------
+
+
 
 
 
 //---------------------------------------------------------
 
+
+
+void Hi(utility& util) {
+	system("cls");
+	int answer = util.Menu("Hi!", "Hi!", "Hi,Hi,Hi,Hi,Hi,Hi,Hi,Hi,Hi,Hi", { "Hi!", {"Hello"}}, util);
+	switch (answer) {
+	case 0:
+		util.nest--;
+		return;
+	case 1:
+		util.nest++;
+		Hello(util);
+	}
+	Hi(util);
+}
+void Hello(utility& util) {
+	system("cls");
+	int answer = util.Menu("Hello!", "Hello!", "Hello,Hello,Hello,Hello,Hi,Hi,Hi,Hi,Hi,Hi");
+	switch (answer) {
+	case 0:
+		util.nest--;
+		return;
+	}
+	Hello(util);
+}
+void Welcome(utility& util) {
+	system("cls");
+	int answer = util.Menu("Welcome!", "Welcome to the payroll system", "The payroll system will contain all the information for a employee and their pay including income tax!", utility::Question("Hello World", { "Hi", "Hello"}));
+	switch (answer) {
+		//case 0 is back or quit
+	case 0:
+		util.nest--;
+		return;
+	case 1:
+		util.nest++;
+		Hi(util);
+		break;
+	case 2:
+		util.nest++;
+		Hello(util);
+		break;
+
+	}
+
+	Welcome(util);
+}
 /*
 The main function will call other helper functions
 which will do the actual calculations and other stuff
 */
 int main() {
-	
+	utility util = utility();
+	Welcome(util);
+
 }
+
